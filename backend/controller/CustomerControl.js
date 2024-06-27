a981875ae3adbed9f6c2142298e86f08b3f9d457
Lachitha <it21019586@my.sliit.lk>
2024-06-27 23:55:41 +0530

    changeConfig

--- backend/controller/CustomerControl.js
+++ backend/controller/CustomerControl.js
@@ -1,259 +1,260 @@
-import Customer from '../model/CustomerModel.js';
-import Token from '../model/Token.js';
-import bcrypt from 'bcrypt';
-import jwt from 'jsonwebtoken';
-import { auth } from '../middleware/auth.js';
-import { sendEmail } from '../utils/sendEmail.js';
-import {  resetPassword } from '../utils/emailTemplate.js';
-
+import Customer from "../model/CustomerModel.js";
+import Token from "../model/Token.js";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import { auth } from "../middleware/auth.js";
+import { sendEmail } from "../utils/sendEmail.js";
+import { resetPassword } from "../utils/emailTemplate.js";
 
 const register = async (req, res) => {
-    try {
-        const existingCustomer = await Customer.findOne({ email: req.body.email });
-        if (existingCustomer) {
-            return res.status(409).json({
-                status: false,
-                message: "Email already exists",
-                data: undefined
-            });
-        }
+	try {
+		console.log("Request body:", req.body);
+		const existingCustomer = await Customer.findOne({ email: req.body.email });
+		if (existingCustomer) {
+			return res.status(409).json({
+				status: false,
+				message: "Email already exists",
+				data: undefined,
+			});
+			console.log("Request body:", req.body);
+		}
 
-        const hashedPassword = await bcrypt.hash(req.body.password, 10);
-        const customer = new Customer({ ...req.body, password: hashedPassword });
-        const savedCustomer = await customer.save();
+		const hashedPassword = await bcrypt.hash(req.body.password, 10);
+		const customer = new Customer({ ...req.body, password: hashedPassword });
+		const savedCustomer = await customer.save();
 
-        return res.status(201).json({
-            status: true,
-            message: "Customer created successfully",
-            data: savedCustomer
-        });
-    } catch (err) {
-        console.error('Error creating customer:', err);
-        return res.status(500).json({
-            status: false,
-            message: err.message,
-            data: undefined
-        });
-    }
+		return res.status(201).json({
+			status: true,
+			message: "Customer created successfully",
+			data: savedCustomer,
+		});
+	} catch (err) {
+		console.error("Error creating customer:", err);
+		return res.status(500).json({
+			status: false,
+			message: err.message,
+			data: undefined,
+		});
+	}
 };
 
 const login = async (req, res) => {
-    try {
-      const customer = await Customer.findOne({ email: req.body.email }).exec();
-      if (!customer) {
-        return res.status(401).json({
-          status: false,
-          message: 'User not found',
-          data: undefined,
-        });
-      }
-  
-      const result = await new Promise((resolve, reject) => {
-        bcrypt.compare(req.body.password, customer.password, (err, result) => {
-          if (err) reject(err);
-          else resolve(result);
-        });
-      });
-  
-      if (result) {
-        const token = jwt.sign(
-          {
-            email: customer.email,
-            customerId: customer._id,
-          },
-          process.env.JWT_KEY,
-          {
-            expiresIn: '1h',
-          }
-        );
-  
-        const tokenDoc = await Token.findOneAndUpdate(
-          { _customerId: customer._id, tokenType: 'login' },
-          { token },
-          { new: true, upsert: true }
-        ).exec();
-  
-        return res.status(200).json({
-          status: true,
-          message: 'Auth successful',
-          data: {
-            token: tokenDoc.token,
-            customer,
-          },
-        });
-      } else {
-        return res.status(401).json({
-          status: true,
-          message: 'Wrong password',
-          data: undefined,
-        });
-      }
-    } catch (err) {
-      console.log(err);
-      res.status(500).json({
-        status: false,
-        message: 'Server Error',
-        data: undefined,
-      });
-    }
-  };
+	try {
+		const customer = await Customer.findOne({ email: req.body.email }).exec();
+		if (!customer) {
+			return res.status(401).json({
+				status: false,
+				message: "User not found",
+				data: undefined,
+			});
+		}
 
-  const logout = async (req, res) => {
-    try {
-     
-      await Token.findOneAndDelete({
-        _customerId: req.customerId,
-        tokenType: 'login',
-      }).exec();
-  
-      return res.status(200).json({
-        status: true,
-        message: 'Logout successful',
-        data: undefined,
-      });
-    } catch (err) {
-      console.log(err);
-      res.status(500).json({
-        status: false,
-        message: 'Server Error',
-        data: undefined,
-      });
-    }
-  };
-  
-  const authUser = async (req, res) => {
-    const customerId = req.customerId;
-    try {
-        const customer = await Customer.findById(customerId);
-        if (!customer) {
-            return res.status(401).json({
-                status: false,
-                message: 'User not found',
-                data: undefined,
-            });
-        }
-        return res.status(200).json({
-            status: true,
-            message: 'User found',
-            data: customer,
-        });
-    } catch (err) {
-        console.error(err);
-        return res.status(500).json({
-            status: false,
-            message: 'Error retrieving user',
-            data: undefined,
-        });
-    }
+		const result = await new Promise((resolve, reject) => {
+			bcrypt.compare(req.body.password, customer.password, (err, result) => {
+				if (err) reject(err);
+				else resolve(result);
+			});
+		});
+
+		if (result) {
+			const token = jwt.sign(
+				{
+					email: customer.email,
+					customerId: customer._id,
+				},
+				process.env.JWT_KEY,
+				{
+					expiresIn: "1h",
+				}
+			);
+
+			const tokenDoc = await Token.findOneAndUpdate(
+				{ _customerId: customer._id, tokenType: "login" },
+				{ token },
+				{ new: true, upsert: true }
+			).exec();
+
+			return res.status(200).json({
+				status: true,
+				message: "Auth successful",
+				data: {
+					token: tokenDoc.token,
+					customer,
+				},
+			});
+		} else {
+			return res.status(401).json({
+				status: true,
+				message: "Wrong password",
+				data: undefined,
+			});
+		}
+	} catch (err) {
+		console.log(err);
+		res.status(500).json({
+			status: false,
+			message: "Server Error",
+			data: undefined,
+		});
+	}
+};
+
+const logout = async (req, res) => {
+	try {
+		await Token.findOneAndDelete({
+			_customerId: req.customerId,
+			tokenType: "login",
+		}).exec();
+
+		return res.status(200).json({
+			status: true,
+			message: "Logout successful",
+			data: undefined,
+		});
+	} catch (err) {
+		console.log(err);
+		res.status(500).json({
+			status: false,
+			message: "Server Error",
+			data: undefined,
+		});
+	}
+};
+
+const authUser = async (req, res) => {
+	const customerId = req.customerId;
+	try {
+		const customer = await Customer.findById(customerId);
+		if (!customer) {
+			return res.status(401).json({
+				status: false,
+				message: "User not found",
+				data: undefined,
+			});
+		}
+		return res.status(200).json({
+			status: true,
+			message: "User found",
+			data: customer,
+		});
+	} catch (err) {
+		console.error(err);
+		return res.status(500).json({
+			status: false,
+			message: "Error retrieving user",
+			data: undefined,
+		});
+	}
 };
 
 const forgetPassword = async (req, res) => {
-    try {
-        const email = req.body.email;
-        const customer = await Customer.findOne({ email });
+	try {
+		const email = req.body.email;
+		const customer = await Customer.findOne({ email });
 
-        if (!customer) {
-            return res.status(401).json({
-                status: false,
-                message: 'User not found',
-                data: undefined,
-            });
-        }
+		if (!customer) {
+			return res.status(401).json({
+				status: false,
+				message: "User not found",
+				data: undefined,
+			});
+		}
 
-        const token = jwt.sign(
-            {
-                email: customer.email,
-                customerId: customer._id,
-            },
-            process.env.JWT_RESET_PW_KEY,
-            {
-                expiresIn: '20m',
+		const token = jwt.sign(
+			{
+				email: customer.email,
+				customerId: customer._id,
+			},
+			process.env.JWT_RESET_PW_KEY,
+			{
+				expiresIn: "20m",
+			}
+		);
 
-            }
-        );
+		const tokenDoc = await Token.findOneAndUpdate(
+			{ _customerId: customer._id, tokenType: "resetPassword" },
+			{ token: token },
+			{ new: true, upsert: true }
+		);
 
-        const tokenDoc = await Token.findOneAndUpdate(
-            { _customerId: customer._id, tokenType: 'resetPassword' },
-            { token: token },
-            { new: true, upsert: true }
-        );
-        
-        if (tokenDoc) {
-            const emailTemplate = resetPassword(email, token);
-            sendEmail(emailTemplate);
-            res.status(200).json({
-                status: true,
-                message: "Email sent successfully",
-                data: undefined
-            });
-        } else {
-            return res.status(401).json({
-                status: false,
-                message: 'Error saving token',
-                data: undefined,
-            });
-        }
-    } catch (err) {
-        console.error(err);
-        return res.status(500).json({
-            status: false,
-            message: 'Error sending email',
-            data: undefined,
-        });
-    }
+		if (tokenDoc) {
+			const emailTemplate = resetPassword(email, token);
+			sendEmail(emailTemplate);
+			res.status(200).json({
+				status: true,
+				message: "Email sent successfully",
+				data: undefined,
+			});
+		} else {
+			return res.status(401).json({
+				status: false,
+				message: "Error saving token",
+				data: undefined,
+			});
+		}
+	} catch (err) {
+		console.error(err);
+		return res.status(500).json({
+			status: false,
+			message: "Error sending email",
+			data: undefined,
+		});
+	}
 };
 
 const resetPasswordcon = async (req, res) => {
-  const token = req.params.token;
-  const password = req.body.password;
+	const token = req.params.token;
+	const password = req.body.password;
 
-  try {
-    const decoded = jwt.verify(token, process.env.JWT_RESET_PW_KEY);
-    const customer = await Token.findOne({ _customerId: decoded.customerId,token: token,tokenType: 'resetPassword',
-    });
-    console.log('customer:', customer);
+	try {
+		const decoded = jwt.verify(token, process.env.JWT_RESET_PW_KEY);
+		const customer = await Token.findOne({
+			_customerId: decoded.customerId,
+			token: token,
+			tokenType: "resetPassword",
+		});
+		console.log("customer:", customer);
 
-    if (!customer) {
-      return res.status(404).json({
-        status: true,
-        message: 'Customer not found or invalid token',
-        data: undefined,
-      });
-    }
+		if (!customer) {
+			return res.status(404).json({
+				status: true,
+				message: "Customer not found or invalid token",
+				data: undefined,
+			});
+		}
 
-    bcrypt.hash(password, 10, async (err, hash) => {
-      if (err) {
-        console.log(err);
-        return res.status(500).json({
-          status: false,
-          message: 'Error hashing password',
-          data: undefined,
-        });
-      }
+		bcrypt.hash(password, 10, async (err, hash) => {
+			if (err) {
+				console.log(err);
+				return res.status(500).json({
+					status: false,
+					message: "Error hashing password",
+					data: undefined,
+				});
+			}
 
-      customer.password = hash;
-      await customer.save();
+			customer.password = hash;
+			await customer.save();
 
-      await Token.findOneAndDelete({
-        _customerId: decoded.customerId,
-        tokenType: 'resetPassword',
-      });
+			await Token.findOneAndDelete({
+				_customerId: decoded.customerId,
+				tokenType: "resetPassword",
+			});
 
-      res.status(200).json({
-        status: true,
-        message: 'Password reset successful',
-        data: undefined,
-      });
-    });
-  } catch (err) {
-    console.log(err);
-    res.status(500).json({
-      status: false,
-      message: 'Error resetting password',
-      data: undefined,
-    });
-  }
+			res.status(200).json({
+				status: true,
+				message: "Password reset successful",
+				data: undefined,
+			});
+		});
+	} catch (err) {
+		console.log(err);
+		res.status(500).json({
+			status: false,
+			message: "Error resetting password",
+			data: undefined,
+		});
+	}
 };
 
-
-export { register, login , logout , authUser, forgetPassword, resetPasswordcon };
+export { register, login, logout, authUser, forgetPassword, resetPasswordcon };
--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,9 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import cors from 'cors';
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
-import CustomerRoutes from './routes/CustomerRoutes.js';
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import CustomerRoutes from "./routes/CustomerRoutes.js";
 
 dotenv.config();
 const app = express();
@@ -11,17 +11,18 @@ app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cors());
-app.use('/', CustomerRoutes);
+app.use("/", CustomerRoutes);
 
 mongoose
-.connect(process.env.MONGODB_URL, { 
-    useNewUrlParser: true, 
-    useUnifiedTopology: true }).then(() => console.log('Connected to MongoDB'))
-                                .catch(err => console.log(err));
-                
-const PORT = process.env.PORT || 1670;
+	.connect(process.env.MONGODB_URL, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+	})
+	.then(() => console.log("Connected to MongoDB"))
+	.catch((err) => console.log(err));
+
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-    console.log(`Server is up and runnig on : ${PORT} 🚀🚀🚀`);
-  });
-  
+	console.log(`Server is up and runnig on : ${PORT} 🚀🚀🚀`);
+});
